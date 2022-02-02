624f8e2b0b449dbc6079a3568db1cf53d7098a72
Andrea Williams <andrea.k.williams7@gmail.com>
2022-02-01 21:54:08 -0800

    Minor changes

--- fishfry-tours/src/PageFooter.js
+++ fishfry-tours/src/PageFooter.js
@@ -8,10 +8,7 @@ export default function PageFooter() {
         <footer>
             <Paper>
                 <Typography>
-                    {process .env.DEV_MODE ? 
-                    (`see <a href="${process.env.API_URL_LOCAL}/graphql">Graph<em>i</em>QL</a> to interact with the GraphQL endpoint`) : 
-                    (`see <a href="${process.env.API_URL_PROD}/graphql">Graph<em>i</em>QL</a> to interact with the GraphQL endpoint`)
-                    }               
+                    see <a href="`${process.env.API_URL_PROD}`/graphql">Graph<em>i</em>QL</a> to interact with the GraphQL endpoint
                 </Typography>
             </Paper>
         </footer>
